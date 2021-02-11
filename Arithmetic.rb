bf39bcd38ecde50968610ce486de623e8b54f2c8
Chaklynkoldyn <fedor.fedor.2905@gmail.com>
2021-02-11 19:05:06 +0200

    add decription

--- Arithmetic.rb
+++ Arithmetic.rb
@@ -1,39 +1,35 @@
-
-
 class Arithmetic
 
-def +(a, b)
-   a + b
- end
+  def +(a, b)
+    a + b
+  end
 
- def -(a, b)
-   a - b
- end
+  def -(a, b)
+    a - b
+  end
 
- def *(a, b)
-   a * b
- end
+  def *(a, b)
+    a * b
+  end
 
- def /(a, b)
-   a / b
- end
+  def /(a, b)
+    a / b
+  end
 
+  def %(a, b)
+    a % b
+  end
 
- def %(a, b)
-   a % b
- end
-
-
- def **(a, b)
-   a ** b
- end
-
+  def **(a, b)
+    a ** b
+  end
 end
+
 arithmetic = Arithmetic.new()
 
-puts arithmetic.+(23, 32)
-puts arithmetic.-(37, 93)
-puts arithmetic.*(12, 12)
-puts arithmetic./(48, 8)
-puts arithmetic.%(7, 4)
-puts arithmetic.**(3, 4)
+puts 'Summa of a and b = ' + arithmetic.+(23, 32).to_s
+puts 'Subtraction of a and b = ' + arithmetic.-(37, 93).to_s
+puts 'Multiplication of a and b = ' + arithmetic.*(12, 12).to_s
+puts 'Division of a and b = ' + arithmetic./(48, 8).to_s
+puts 'The remainder of division of a and b = ' + arithmetic.%(7, 4).to_s
+puts 'a to b degrees = ' + arithmetic.**(3, 4).to_s
