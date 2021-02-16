5a562f057cb6aadcd7026db8bba60095984c49fd
Chaklynkoldyn <fedor.fedor.2905@gmail.com>
2021-02-16 16:47:34 +0200

    my array

---
+++ my_array.rb
@@ -0,0 +1,63 @@
+class MyArray
+  attr_accessor :array
+
+  def initialize(arr = [])
+    @array = arr
+  end
+
+  def size
+   arr.size
+  end
+
+  def reverse
+    arr.reverse
+  end
+
+  def max
+    arr.max
+  end
+
+  def min
+    arr.min
+  end
+
+  def desc
+    arr.desc
+  end
+
+  def asc
+    arr.asc
+  end
+
+  def odd
+    arr.odd
+  end
+
+  def multiple_to_three
+    arr.multiple_to_three
+  end
+
+  def uniq
+    arr.uniq
+  end
+
+  def devide_on_ten
+    arr.devide_on_ten
+  end
+
+  def chars
+    arr.chars
+  end
+
+  def switch
+    arr.switch
+  end
+
+  def before_min
+    arr.before_min
+  end
+
+  def three_smallest
+    arr.three_smallest
+  end
+end
